4a9e29f2e7a13fab09c7040ee45c3ddc6947ae0d
shashik thiwanka <shashik thiwanka>
2016-11-05 13:20:47 +0530

    first commit

---
+++ resources/views/includes/message-block.blade.php
@@ -0,0 +1,22 @@
+@if(count($errors)>0)
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+
+        </div>
+
+    </div>
+@endif
+@if(Session::has('message'))
+
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 alert-success">
+           {{Session::get('message')}}
+        </div>
+
+    </div>
+@endif
